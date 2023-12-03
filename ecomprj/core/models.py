24a76af5c5b9e16e609ef4e4430da48bb0432eb7
Destiny Franks <desphixs@gmail.com>
2023-12-03 15:15:13 +0100

    Updated CKEDITOR to v5

--- ecomprj/core/models.py
+++ ecomprj/core/models.py
@@ -3,7 +3,7 @@ from shortuuid.django_fields import ShortUUIDField
 from django.utils.html import mark_safe
 from userauths.models import User
 from taggit.managers import TaggableManager
-from ckeditor_uploader.fields import  RichTextUploadingField
+from django_ckeditor_5.fields import CKEditor5Field
 
 
 STATUS_CHOICE = (
@@ -22,23 +22,22 @@ STATUS = (
 )
 
 
-
 RATING = (
-    ( 1,  "★☆☆☆☆"),
-    ( 2,  "★★☆☆☆"),
-    ( 3,  "★★★☆☆"),
-    ( 4,  "★★★★☆"),
-    ( 5,  "★★★★★"),
+    (1,  "★☆☆☆☆"),
+    (2,  "★★☆☆☆"),
+    (3,  "★★★☆☆"),
+    (4,  "★★★★☆"),
+    (5,  "★★★★★"),
 )
-
-
 
 
 def user_directory_path(instance, filename):
     return 'user_{0}/{1}'.format(instance.user.id, filename)
 
+
 class Category(models.Model):
-    cid = ShortUUIDField(unique=True, length=10, max_length=20, prefix="cat", alphabet="abcdefgh12345")
+    cid = ShortUUIDField(unique=True, length=10, max_length=20,
+                         prefix="cat", alphabet="abcdefgh12345")
     title = models.CharField(max_length=100, default="Food")
     image = models.ImageField(upload_to="category", default="category.jpg")
 
@@ -51,18 +50,22 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+
 class Tags(models.Model):
     pass
 
 
 class Vendor(models.Model):
-    vid = ShortUUIDField(unique=True, length=10, max_length=20, prefix="ven", alphabet="abcdefgh12345")
+    vid = ShortUUIDField(unique=True, length=10, max_length=20,
+                         prefix="ven", alphabet="abcdefgh12345")
 
     title = models.CharField(max_length=100, default="Nestify")
-    image = models.ImageField(upload_to=user_directory_path, default="vendor.jpg")
-    cover_image = models.ImageField(upload_to=user_directory_path, default="vendor.jpg")
+    image = models.ImageField(
+        upload_to=user_directory_path, default="vendor.jpg")
+    cover_image = models.ImageField(
+        upload_to=user_directory_path, default="vendor.jpg")
     # description = models.TextField(null=True, blank=True, default="I am am Amazing Vendor")
-    description = RichTextUploadingField(null=True, blank=True, default="I am am Amazing Vendor")
+    description = CKEditor5Field(config_name='extends', null=True, blank=True)
 
     address = models.CharField(max_length=100, default="123 Main Street.")
     contact = models.CharField(max_length=100, default="+123 (456) 789")
@@ -86,44 +89,53 @@ class Vendor(models.Model):
 
 
 class Product(models.Model):
-    pid = ShortUUIDField(unique=True, length=10, max_length=20, alphabet="abcdefgh12345")
+    pid = ShortUUIDField(unique=True, length=10,
+                         max_length=20, alphabet="abcdefgh12345")
 
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name="category")
-    vendor = models.ForeignKey(Vendor, on_delete=models.SET_NULL, null=True, related_name="product")
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, related_name="category")
+    vendor = models.ForeignKey(
+        Vendor, on_delete=models.SET_NULL, null=True, related_name="product")
 
     title = models.CharField(max_length=100, default="Fresh Pear")
-    image = models.ImageField(upload_to=user_directory_path, default="product.jpg")
+    image = models.ImageField(
+        upload_to=user_directory_path, default="product.jpg")
     # description = models.TextField(null=True, blank=True, default="This is the product")
-    description = RichTextUploadingField(null=True, blank=True, default="This is the product")
+    description = CKEditor5Field(config_name='extends', null=True, blank=True)
 
-    price = models.DecimalField(max_digits=99999999999999, decimal_places=2, default="1.99")
-    old_price = models.DecimalField(max_digits=99999999999999, decimal_places=2, default="2.99")
+    price = models.DecimalField(
+        max_digits=99999999999999, decimal_places=2, default="1.99")
+    old_price = models.DecimalField(
+        max_digits=99999999999999, decimal_places=2, default="2.99")
 
-    specifications = RichTextUploadingField(null=True, blank=True)
+    specifications = CKEditor5Field(config_name='extends', null=True, blank=True)
     # specifications = models.TextField(null=True, blank=True)
-    type = models.CharField(max_length=100, default="Organic", null=True, blank=True)
-    stock_count = models.CharField(max_length=100, default="10", null=True, blank=True)
-    life = models.CharField(max_length=100, default="100 Days", null=True, blank=True)
+    type = models.CharField(
+        max_length=100, default="Organic", null=True, blank=True)
+    stock_count = models.CharField(
+        max_length=100, default="10", null=True, blank=True)
+    life = models.CharField(
+        max_length=100, default="100 Days", null=True, blank=True)
     mfd = models.DateTimeField(auto_now_add=False, null=True, blank=True)
 
     tags = TaggableManager(blank=True)
 
     # tags = models.ForeignKey(Tags, on_delete=models.SET_NULL, null=True)
 
-    product_status = models.CharField(choices=STATUS, max_length=10, default="in_review")
+    product_status = models.CharField(
+        choices=STATUS, max_length=10, default="in_review")
 
     status = models.BooleanField(default=True)
     in_stock = models.BooleanField(default=True)
     featured = models.BooleanField(default=False)
     digital = models.BooleanField(default=False)
 
-    sku = ShortUUIDField(unique=True, length=4, max_length=10, prefix="sku", alphabet="1234567890")
+    sku = ShortUUIDField(unique=True, length=4, max_length=10,
+                         prefix="sku", alphabet="1234567890")
 
     date = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(null=True, blank=True)
-
-
 
     class Meta:
         verbose_name_plural = "Products"
@@ -140,33 +152,32 @@ class Product(models.Model):
 
 
 class ProductImages(models.Model):
-    images = models.ImageField(upload_to="product-images", default="product.jpg")
-    product = models.ForeignKey(Product, related_name="p_images", on_delete=models.SET_NULL, null=True)
+    images = models.ImageField(
+        upload_to="product-images", default="product.jpg")
+    product = models.ForeignKey(
+        Product, related_name="p_images", on_delete=models.SET_NULL, null=True)
     date = models.DateTimeField(auto_now_add=True)
-
 
     class Meta:
         verbose_name_plural = "Product Images"
 
 
-
-
-
 ############################################## Cart, Order, OrderITems and Address ##################################
 ############################################## Cart, Order, OrderITems and Address ##################################
 ############################################## Cart, Order, OrderITems and Address ##################################
 ############################################## Cart, Order, OrderITems and Address ##################################
-
-
 
 
 class CartOrder(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=99999999999999, decimal_places=2, default="1.99")
+    price = models.DecimalField(
+        max_digits=99999999999999, decimal_places=2, default="1.99")
     paid_status = models.BooleanField(default=False, null=True, blank=True)
     order_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    product_status = models.CharField(choices=STATUS_CHOICE, max_length=30, default="processing")
-    sku = ShortUUIDField(null=True, blank=True, length=5, prefix="SKU", max_length=20, alphabet="abcdefgh12345")
+    product_status = models.CharField(
+        choices=STATUS_CHOICE, max_length=30, default="processing")
+    sku = ShortUUIDField(null=True, blank=True, length=5,
+                         prefix="SKU", max_length=20, alphabet="abcdefgh12345")
 
     class Meta:
         verbose_name_plural = "Cart Order"
@@ -179,31 +190,28 @@ class CartOrderProducts(models.Model):
     item = models.CharField(max_length=200)
     image = models.CharField(max_length=200)
     qty = models.IntegerField(default=0)
-    price = models.DecimalField(max_digits=99999999999999, decimal_places=2, default="1.99")
-    total = models.DecimalField(max_digits=99999999999999, decimal_places=2, default="1.99")
-
+    price = models.DecimalField(
+        max_digits=99999999999999, decimal_places=2, default="1.99")
+    total = models.DecimalField(
+        max_digits=99999999999999, decimal_places=2, default="1.99")
 
     class Meta:
         verbose_name_plural = "Cart Order Items"
-
 
     def order_img(self):
         return mark_safe('<img src="/media/%s" width="50" height="50" />' % (self.image))
 
 
-
-
 ############################################## Product Revew, wishlists, Address ##################################
 ############################################## Product Revew, wishlists, Address ##################################
 ############################################## Product Revew, wishlists, Address ##################################
 ############################################## Product Revew, wishlists, Address ##################################
-
-
 
 
 class ProductReview(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, related_name="reviews")
+    product = models.ForeignKey(
+        Product, on_delete=models.SET_NULL, null=True, related_name="reviews")
     review = models.TextField()
     rating = models.IntegerField(choices=RATING, default=None)
     date = models.DateTimeField(auto_now_add=True)
@@ -216,7 +224,6 @@ class ProductReview(models.Model):
 
     def get_rating(self):
         return self.rating
-
 
 
 class wishlist_model(models.Model):
